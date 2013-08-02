3ee45f293b7b65470a3dc98ef6e7db871bc17011
Ricardo Dantas <ricardodantas@gmail.com>
2013-08-02 14:35:06 -0300

    update

--- extb-slider.js
+++ extb-slider.js
@@ -24,10 +24,10 @@
     $.fn.extbSlider = function(settings) {
 
         var $father_element = this;
-
+        var numberOfSlides = $father_element.children(config.children).length;
 
         var config = $.extend({
-            width : '100%',
+            width : 500, // pixes
             children : $father_element.children(),
             max_view : 4
 
@@ -39,63 +39,68 @@
             if ($father_element.children(config.children).length > 0){
 
                 $father_element.children(config.children).hide();
-                $father_element.children(config.children).eq(0).show();
+                // $father_element.children(config.children).first().show();
 
                 $father_element.children(config.children).each(function(i){
-                        $(this).addClass('extbSliderItem slide_'+(i+1));
+                        order = i+1;
+                        $(this).addClass('extbSliderItem slide_'+order);
+
+                        if(order <= config.max_view){
+                            $(this).show();
+                        }
                 });
 
-                $father_element.before('<div class="extbSliderBtBack"><a class="extbSliderLnkBack" href="javascript:void(0);">Back</a></div>');
-                $father_element.after('<div class="extSliderbBtNext"><a class="extbSliderLnkNext" href="javascript:void(0);">Next</a></div>');
+                $father_element.children('.extbSliderItem').wrapAll('<div class="extbSliderWrapAll" />');
+                var $extbSliderWrapAll = $father_element.children('.extbSliderWrapAll');
 
-                var $current_children = $father_element.children(config.children).filter(':visible');
+                $extbSliderWrapAll.css('width', config.width * config.max_view);
+
+                $extbSliderWrapAll.before('<div class="extbSliderBtBack"><a class="extbSliderLnkBack" href="javascript:void(0);">Back</a></div>');
+                $extbSliderWrapAll.after('<div class="extSliderbBtNext"><a class="extbSliderLnkNext" href="javascript:void(0);">Next</a></div>');
+
+                var $current_children = $extbSliderWrapAll.children(config.children).filter(':visible');
                 var $previous_children = $current_children.prev();
                 var $next_children = $current_children.next();
 
 
-                $('.extbSliderBtBack a').bind('click',function(e){
+                $father_element.find('.extbSliderBtBack a').bind('click',function(e){
 
                     e.preventDefault();
 
                     if( $previous_children.hasClass('extbSliderItem') ){
 
-                        $father_element.children(config.children).hide();
+                        $extbSliderWrapAll.children(config.children).hide();
                         $previous_children.fadeIn('slow');
 
-                        // $(this).removeClass('disabled');
+
 
                     }else{
-                        $father_element.children(config.children).hide();
-                        $father_element.children(config.children).last().fadeIn('slow');
-                        // $(this).addClass('disabled');
+                        $extbSliderWrapAll.children(config.children).hide();
+                        $extbSliderWrapAll.children(config.children).last().fadeIn('slow');
+
                     }
-                    $current_children = $father_element.children(config.children).filter(':visible');
+                    $current_children = $extbSliderWrapAll.children(config.children).filter(':visible');
                     $previous_children = $current_children.prev();
                     $next_children = $current_children.next();
-                    // console.log('prev: ' + $previous_children.attr('class'));
-                    // console.log('next: ' + $next_children.attr('class'));
 
                 });
 
-                $('.extSliderbBtNext a').bind('click',function(e){
+                $father_element.find('.extSliderbBtNext a').bind('click',function(e){
 
                     e.preventDefault();
                     if( $next_children.hasClass('extbSliderItem') ){
 
-                        $father_element.children(config.children).hide();
+                        $extbSliderWrapAll.children(config.children).hide();
                         $next_children.fadeIn('slow');
 
                     }else{
-                        $father_element.children(config.children).hide();
-                        $father_element.children(config.children).first().fadeIn('slow');
+                        $extbSliderWrapAll.children(config.children).hide();
+                        $extbSliderWrapAll.children(config.children).first().fadeIn('slow');
                     }
 
-                    $current_children = $father_element.children(config.children).filter(':visible');
+                    $current_children = $extbSliderWrapAll.children(config.children).filter(':visible');
                     $previous_children = $current_children.prev();
                     $next_children = $current_children.next();
-
-                    // console.log('prev: ' + $previous_children.attr('class'));
-                    // console.log('next: ' + $next_children.attr('class'));
 
                 });
 
